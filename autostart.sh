e0c27b8b5066fc97afc65211b65c405258eaaeac
Justin Seymour <jtseymour99@gmail.com>
2020-09-21 14:27:59 +1000

    Replaced caps indicator in bar with program

--- autostart.sh
+++ autostart.sh
@@ -4,10 +4,12 @@ syndaemon -i 0.8 -K -d -R -t &
 
 picom &
 
-feh --bg-scale ~/Pictures/uenhk4759a831.jpg &
+feh --no-fehbg --bg-scale ~/Pictures/uenhk4759a831.jpg &
 
 nm-applet &
 
 udiskie -t &
 
 exec ~/bin/dwmstatus.sh &
+
+~/documents/rcode/caps_indicator/caps
--- bin/dwmstatus.sh
+++ bin/dwmstatus.sh
@@ -70,16 +70,16 @@ spot() {
     echo $symbol $title - $artist $sep
 }
 
-caps() {
-
-    temp=""
-
-    [ $(xset -q | grep Caps | awk '{print $4}') = "on" ] && temp="CAPS"
-
-    echo $temp $sep
-}
+#caps() {
+#
+#    temp=""
+#
+#    [ $(xset -q | grep Caps | awk '{print $4}') = "on" ] && temp="CAPS"
+#
+#    [[ "$temp" = "CAPS" ]] && echo $temp $sep ""
+#}
 
 while true; do
-    xsetroot -name "$(spot) $(caps) $(mem) $sep $(bat) $sep $(vol) $sep $(dte)"
-    sleep 2s
+    xsetroot -name "$(spot) $(mem) $sep $(bat) $sep $(vol) $sep $(dte)"
+    sleep 1s
 done &
