25509336627a96e7f86401e6361d150d18757d48
Justin Seymour <jtseymour99@gmail.com>
2022-09-23 17:14:42 +1000

    Added nvim config

--- getfiles.sh
+++ getfiles.sh
@@ -8,10 +8,9 @@ cp -r ~/bin .
 cp ~/.dwm/autostart.sh .
 cp -r ~/.config/alacritty .config/.
 cp -r ~/.config/dunst .config/.
-cp -r ~/.config/ranger .config/.
-cp -r ~/.config/vifm .config/.
 cp -r ~/.config/git .config/.
 cp -r ~/.config/fontconfig .config/.
 cp -r ~/.config/neofetch .config/.
 cp ~/.config/mimeapps.list .config/.
 cp -r ~/.config/picom/ .config/.
+cp -r ~/.config/nvim .config/.
